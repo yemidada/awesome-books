2d86928fc91e2f4dc0b5eb366380dde394799c04
yemidada <yemidada@gmail.com>
2023-05-29 15:29:05 +0100

    update script file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,9 @@
 const addBookBtn = document.querySelector('.add-book-btn');
-const removeBookBtn = document.querySelector('.remove-book-btn');
+const removeBookBtn = document.querySelectorAll('.remove-book-btn');
 
+/*
+    Form
+*/
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
 const errorMessage = document.querySelector('#error-message');
@@ -9,11 +12,7 @@ const bookList = document.querySelector('#books-list');
 
 const booksStorage = 'storage';
 
-const books = [
-
-];
-
-const setFormChange = () => {
+const setFormChange = (books) => {
   localStorage.setItem(booksStorage, JSON.stringify(books));
 };
 
@@ -23,7 +22,11 @@ const onloadContent = () => {
     const booksJson = JSON.parse(booksData);
     bookList.innerHTML = '';
     for (let i = 0; i < booksJson.length; i += 1) {
-      const html = '';
+      let html = '<ul>';
+      html += `<li>${booksJson[i].title}</li>`;
+      html += `<li>${booksJson[i].author}</li>`;
+      html += `<li><button type="button" class="remove-book-btn" data-id="${i}">Remove</button></li>`;
+      html += '</ul>';
       bookList.innerHTML += html;
     }
   }
@@ -39,16 +42,28 @@ addBookBtn.addEventListener('click', () => {
   } else if (!bookAuthor.value) {
     errorMessage.innerHTML = 'Book author is required';
   } else {
-    books.push({
-      author: bookAuthor.value,
-      title: bookTitle.value,
-    });
-    setFormChange();
+    const booksData = localStorage.getItem(booksStorage);
+    if (booksData) {
+      const booksJson = JSON.parse(booksData);
+      booksJson.push({
+        author: bookAuthor.value,
+        title: bookTitle.value,
+      });
+      setFormChange(booksJson);
+      onloadContent();
+    }
   }
 });
 
-removeBookBtn.addEventListener('click', (e) => {
-  const index = e.currentTarget.dataset.id;
-  books.splice(index, 1);
-  setFormChange();
+removeBookBtn.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const index = e.currentTarget.dataset.id;
+    const booksData = localStorage.getItem(booksStorage);
+    if (booksData) {
+      const booksJson = JSON.parse(booksData);
+      booksJson.splice(index, 1);
+      setFormChange(booksJson);
+      onloadContent();
+    }
+  });
 });
